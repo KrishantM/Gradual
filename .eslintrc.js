0e22f95536134a3f814d2c3dd980d101a7c48f8f
KrishantM <krishantm7@gmail.com>
2025-06-30 12:48:53 +1200

    Removed .eslintrc.js and updated eslint.config.mjs for deploy

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: 'next',
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
-  };
-  
