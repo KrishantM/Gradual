95d7b82128c024c85dd42149318ee652ba4b2543
KrishantM <krishantm7@gmail.com>
2025-07-12 15:22:50 +1200

    Fix unescaped apostrophe in login page for successful build

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -143,7 +143,7 @@ export default function LoginPage() {
                     <div className="w-full border-t border-white/20"></div>
                   </div>
                   <div className="relative flex justify-center text-sm">
-                    <span className="px-2 bg-transparent text-gray-400">Don't have an account?</span>
+                    <span className="px-2 bg-transparent text-gray-400">Don&apos;t have an account?</span>
                   </div>
                 </div>
 
